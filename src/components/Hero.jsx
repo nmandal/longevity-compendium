5ee6e823597666b04167b526b3cb9a33019b4a67
nmandal <nicholas.mandal@gmail.com>
2022-09-20 10:05:04 -0500

    github link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -57,7 +57,10 @@ export function Hero() {
               </p>
               <div className="mt-8 flex gap-4 md:justify-center lg:justify-start">
                 <Button href="/">Get started</Button>
-                <Button href="/" variant="secondary">
+                <Button
+                  href="https://github.com/nmandal/longevity-compendium"
+                  variant="secondary"
+                >
                   View on GitHub
                 </Button>
               </div>
